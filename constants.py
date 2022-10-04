e7025371ca784f288e4b36c6c371bdfefd43dc53
Vincenzo Fehring <vincenzo.fehring@me.com>
2022-10-04 02:30:00 -0500

    Added some more social gifs

--- constants.py
+++ constants.py
@@ -12,14 +12,40 @@ DROP_LOCATIONS = []
 CURRENCY_EMOJI = "<:gold_coin:973071732858585088>"
 
 # SOCIAL GIFS
-FUCK_GIFS = []
-LICK_GIFS = []
-KISS_GIFS = []
-KICK_GIFS = []
-SPANK_GIFS = []
-HUG_GIFS = []
-SLAP_GIFS = []
-RESTRAIN_GIFS = []
-CUDDLE_GIFS = []
-PET_GIFS = []
-PUNCH_GIFS = []
+FUCK_GIFS = ['https://c.tenor.com/qC7OMlSm00EAAAAC/sex-love.gif',
+             'https://media.tenor.com/jleVaFqBaP4AAAAd/excited-anime.gif']
+LICK_GIFS = ['https://c.tenor.com/cYDXPpX6Wf8AAAAC/tongue-lick.gif',
+             'https://media.tenor.com/Pb1JPfqXpAIAAAAC/lick-licky.gif',
+             'https://media.tenor.com/S5I9g4dPRn4AAAAC/omamori-himari-manga.gif',
+             'https://media.tenor.com/zIU_JbsnMQ8AAAAC/zatch-bell-golden-gash.gif']
+KISS_GIFS = ['https://c.tenor.com/fnPi9nXCgzwAAAAC/kiss-lip.gif',
+             'https://media.tenor.com/cL7b2RYjAfAAAAAC/hot-love.gif',
+             'https://media.tenor.com/VxNAxTOeT3YAAAAC/anime-kiss.gif']
+KICK_GIFS = ['https://c.tenor.com/Eb8kmWpYw3wAAAAC/kirk-star.gif',
+             'https://media.tenor.com/CJj7Ik-0zFEAAAAC/navibot-kick.gif']
+SPANK_GIFS = ['https://c.tenor.com/wnnI2YFACBYAAAAC/spank-ass.gif',
+              'https://media.tenor.com/LMKZH2bDKHsAAAAC/spank-anime.gif',
+              'https://media.tenor.com/Sp7yE5UzqFMAAAAC/spank-slap.gif',
+              'https://media.tenor.com/NBmB6PIpX5sAAAAC/rosario-to-vampire-spanking.gif']
+HUG_GIFS = ['https://c.tenor.com/mB_y2KUsyuoAAAAd/cuddle-anime-hug.gif']
+SLAP_GIFS = ['https://c.tenor.com/EzwsHlQgUo0AAAAC/slap-in-the-face-angry.gif',
+             'https://media.tenor.com/XiYuU9h44-AAAAAC/anime-slap-mad.gif',
+             'https://media.tenor.com/eU5H6GbVjrcAAAAC/slap-jjk.gif',
+             'https://media.tenor.com/LUJRVpYgy-8AAAAC/kiniro-kiniro-mosaic.gif',
+             'https://media.tenor.com/yJmrNruFNtEAAAAC/slap.gif']
+RESTRAIN_GIFS = ['https://c.tenor.com/oc6RP1L4SZcAAAAC/tied-up-freaks-youre-one-of-us.gif',
+                 'https://media.tenor.com/EawRHVGUtkkAAAAC/makenki-anime.gif',
+                 'https://media.tenor.com/HJuPOfpqDd0AAAAC/anime.gif',
+                 'https://media.tenor.com/8GzqNCkt_hIAAAAC/jessica-rabbit-bound.gif']
+CUDDLE_GIFS = ['https://c.tenor.com/3eVUj2WnOBkAAAAC/love-husband.gif',
+               'https://media.tenor.com/SNLQJ7-fupUAAAAC/goals-pillow.gif']
+PET_GIFS = ['https://c.tenor.com/nwbxEGQINOsAAAAd/pet-dog.gif',
+            'https://media.tenor.com/sX-K9XVf6KoAAAAC/catgirl-neko.gif',
+            'https://media.tenor.com/hmzhWv3b9KsAAAAd/anime-head-pat-anime-head-rub.gif',
+            'https://media.tenor.com/rZRQ6gSf128AAAAC/anime-good-girl.gif',
+            'https://media.tenor.com/bfpRP4Feg3oAAAAC/pet-anime.gif']
+PUNCH_GIFS = ['https://c.tenor.com/1Sd82w25kacAAAAC/one-punch-man-punch.gif',
+              'https://c.tenor.com/FFYqOVVbrJAAAAAC/markiplier-punch.gif',
+              'https://c.tenor.com/p_mMicg1pgUAAAAC/anya-forger-damian-spy-x-family.gif',
+              'https://media.tenor.com/BoYBoopIkBcAAAAC/anime-smash.gif',
+              'https://media.tenor.com/6a42QlkVsCEAAAAd/anime-punch.gif']
